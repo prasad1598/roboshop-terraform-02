669f948981f8c43a475cd8a7dd5a39098e8a21b9
muniprasad <muniprasaddiddi@gmail.com>
2025-05-01 12:19:40 +0530

    files updated

---
+++ session-21(2)/main.tf
@@ -0,0 +1,24 @@
+module "databases" {
+  for_each                   = var.databases
+  source                     = "./component"
+  ip_configuration_subnet_id = var.ip_configuration_subnet_id
+  # location                   = var.location
+  network_security_group_id  = var.network_security_group_id
+  name                       = each.key
+  rg_name                    = var.rg_name
+  storage_image_reference_id = var.storage_image_reference_id
+  zone_name                  = var.zone_name
+}
+
+module "application" {
+  depends_on                 = [module.databases]
+  for_each                   = var.applications
+  source                     = "./component"
+  ip_configuration_subnet_id = var.ip_configuration_subnet_id
+  # location                   = var.location
+  network_security_group_id  = var.network_security_group_id
+  name                       = each.key
+  rg_name                    = var.rg_name
+  storage_image_reference_id = var.storage_image_reference_id
+  zone_name                  = var.zone_name
+}
---
+++ session-21(2)/variable.tf
@@ -0,0 +1,44 @@
+variable "rg_name" {
+  default = "Project_RG"
+}
+
+variable "ip_configuration_subnet_id" {
+  default = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/Project_RG/providers/Microsoft.Network/virtualNetworks/Project_VN/subnets/default"
+}
+
+variable "network_security_group_id" {
+  default = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/Project_RG/providers/Microsoft.Network/networkSecurityGroups/Project_NSG"
+}
+
+variable "storage_image_reference_id" {
+  default = "/subscriptions/aa32da49-0603-4855-b55b-bfd4bcf7b16f/resourceGroups/Project_RG/providers/Microsoft.Compute/images/test-devops-practice"
+}
+
+variable "zone_name" {
+  default = "prasaddevops.shop"
+}
+
+# variable "location" {
+#   default = "UK West"
+# }
+
+variable "databases" {
+  default = {
+    mongodb = {}
+    rabbitmq = {}
+    mysql = {}
+    redis = {}
+  }
+}
+
+variable "applications" {
+  default = {
+    catalogue = {}
+    user = {}
+    cart = {}
+    payment = {}
+    shipping = {}
+    dispatch = {}
+    frontend = {}
+  }
+}
